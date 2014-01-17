9cdd5c49a37c47f7ec34092e44f79d4de9c0f428
36V3mA21Hz <ahzjx1989@gmail.com>
2014-01-17 12:35:51 -0500

    new

--- Codes/MaxSubarray.cpp
+++ Codes/MaxSubarray.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int maxSubArray(int A[], int n) {
-    	int tempSum, sum, index;
-    	tempSum = sum = A[0];
-    	for (int i = 1; i < n; i++) {
-    		if (tempSum + A[i] > sum) {
-    			sum = tempSum + A[i];
-    			index = i;
-    		}
-    		tempSum += A[i];
-    
-    	}
-    	tempSum = A[index];
-    	for (int i = index - 1; i >= 0; i--) {
-    		if (tempSum + A[i] > sum) sum = tempSum + A[i];
-    		tempSum += A[i];
-    	}
-    	return sum;
+        int sum, tempSum;
+        sum = tempSum = A[0];
+        for (int i = 1; i < n; i++) {
+            if (tempSum < 0) {
+                tempSum = A[i];
+            } else {
+                tempSum += A[i];
+            }
+            if (sum <= tempSum) sum = tempSum;
+        }
+
+        return sum;
     }
 };
--- Codes/PascalTriangle.cpp
+++ Codes/PascalTriangle.cpp
@@ -1,23 +1,20 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-vector<vector<int> > generate(int numRows) {
-	if (numRows == 0) return{ {} };
-	vector<vector<int>> pascal(numRows, {});
-	for (int j = 1; j < numRows; j++) {
-		for (int i = 0; i < j + 1; i++) {
-			int x;
-			if (i != 0 && i != j)
-				x = pascal[j - 1][i] + pascal[j - 1][i - 1];
-			else
-				x = 1;
-			pascal[j].push_back(x);
+class Solution {
+public:
+	vector<vector<int> > generate(int numRows) {
+		if (numRows == 0) return{};
+		vector<int> blank;
+		vector<vector<int>> pascal(numRows, blank);
+		pascal[0].push_back(1);
+		for (int j = 1; j < numRows; j++) {
+			for (int i = 0; i < j + 1; i++) {
+				int x;
+				if (i != 0 && i != j)
+					x = pascal[j - 1][i] + pascal[j - 1][i - 1];
+				else
+					x = 1;
+				pascal[j].push_back(x);
+			}
 		}
+		return pascal;
 	}
-	return pascal;
-}
-
-int main() {
-	vector<vector<int> > result = generate(5);
-	return 0;
-}
+};
