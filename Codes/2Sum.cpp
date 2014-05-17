2cac7585746afe104ae5224901cd43bbaf7f9502
Julian Zhou <junzhou365@gmail.com>
2014-05-16 23:33:18 -0400

    new

---
+++ Codes/2Sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {
+        if (numbers.empty())
+        	return {};
+        int n = (int)numbers.size();
+        int i = 0, j = n - 1;
+        unordered_map<int, int> mymap;
+        for (int k = 0; k < n; k++) {
+        	pair<int, int> temp(numbers[k], k);
+        	mymap.insert(temp);
+        }
+        sort (numbers.begin(), numbers.end());
+        while (i < j) {
+        	if (numbers[i] + numbers[j] == target) {
+        		auto g1 = mymap.find(numbers[i]);
+        		auto g2 = mymap.find(numbers[j]);
+        		return {g1->second, g2->second};
+        	} else if (numbers[i] + numbers[j] < target) {
+        		i++;
+        	} else {
+        		j--;
+        	}
+        }
+        return {};
+    }
+};
