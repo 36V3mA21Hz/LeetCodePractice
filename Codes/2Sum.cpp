b0fa39fc2505d73a052920dca3dda36ddfdf6ac2
Julian Zhou <junzhou365@gmail.com>
2014-05-18 12:17:56 -0400

    new

--- Codes/2Sum.cpp
+++ Codes/2Sum.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int> &numbers, int target) {
-        if (numbers.empty())
-            return {};
-        int n = (int)numbers.size();
-        int i = 0, j = n - 1;
         unordered_map<int, int> mymap;
-        for (int k = 0; k < n; k++) {
-            pair<int, int> temp(numbers[k], k);
-            mymap.insert(temp);
+        int n = (int)numbers.size();
+        for (int i = 0; i < n; i++) {
+            mymap[numbers[i]] = i;
         }
-        sort (numbers.begin(), numbers.end());
-        while (i < j) {
-            if (numbers[i] + numbers[j] == target) {
-                auto g1 = mymap.find(numbers[i]);
-                auto g2 = mymap.find(numbers[j]);
-                if (g1->second < g2->second)
-                    return {g1->second + 1, g2->second + 1};
-                else
-                    return {g2->second + 1, g1->second + 1};
-            } else if (numbers[i] + numbers[j] < target) {
-                i++;
-            } else {
-                j--;
+        for (int i = 0; i < n; i++) {
+            int counterpart = target - numbers[i];
+            auto got = mymap.find(counterpart);
+            if (got != mymap.end()) {
+                if (got->second != i)
+                    return {i + 1, got->second + 1};
             }
         }
         return {};
